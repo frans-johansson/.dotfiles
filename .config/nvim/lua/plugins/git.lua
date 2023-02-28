de42f7c2b2fdfc6513487a89c3d21eef076e10b8
Frans Johansson <fransjohansson98@gmail.com>
2023-02-28 20:28:37 +0100

    Update git-conflict config

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -6,8 +6,19 @@ return {
 	},
 	{
 		"akinsho/git-conflict.nvim",
-		version = "*",
-		opts = {},
+		commit = "2957f74",
+		config = function()
+			require("git-conflict").setup({
+				default_mappings = {
+					ours = "co",
+					theirs = "ct",
+					none = "c0",
+					both = "cb",
+					next = "cn",
+					prev = "cp",
+				},
+			})
+		end,
 	},
 	"tpope/vim-fugitive",
 }

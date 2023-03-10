2b7bc151ec780983e79d3168dff3393c0b36aefb
Frans Johansson <fransjohansson98@gmail.com>
2023-03-10 15:24:44 +0100

    Update lualine to use rounded corners

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -9,7 +9,19 @@ return {
 				icons_enabled = true,
 				theme = lualine_theme,
 				component_separators = "|",
-				section_separators = "",
+				section_separators = { left = '', right = '' },
+			},
+			sections = {
+				lualine_a = {
+					{ 'mode', separator = { left = '' }, right_padding = 2 },
+				},
+				lualine_b = { 'filename', 'branch' },
+				lualine_c = { 'fileformat' },
+				lualine_x = {},
+				lualine_y = { 'filetype', 'progress' },
+				lualine_z = {
+					{ 'location', separator = { right = '' }, left_padding = 2 },
+				},
 			},
 		})
 	end,
